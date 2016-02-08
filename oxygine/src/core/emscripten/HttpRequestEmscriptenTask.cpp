1e21e305cc9065f1f13d0d31976cf1bce5f60eb6
dmuratshin <frankinshtein85@gmail.com>
2016-02-08 22:49:23 +0500

    fixed emscripten http request minor

--- oxygine/src/core/emscripten/HttpRequestEmscriptenTask.cpp
+++ oxygine/src/core/emscripten/HttpRequestEmscriptenTask.cpp
@@ -43,9 +43,11 @@ namespace oxygine
         addRef();
 
         log::messageln("HttpRequestEmscriptenTask::_run %s", _url.c_str());
-        _postData.push_back(0);
+        const char* method = _postData.empty() ? "GET" : "POST";
+        if (!_postData.empty())
+            _postData.push_back(0);
 
-        emscripten_async_wget2_data(_url.c_str(), _postData.empty() ? "GET" : "POST", (char*)&_postData.front(), this, false, onload, onerror, onprogress);
+        emscripten_async_wget2_data(_url.c_str(), method, (char*)&_postData.front(), this, false, onload, onerror, onprogress);
     }
     //int  emscripten_async_wget2_data(const char* url, const char* requesttype, const char* param, void *arg, int free, em_async_wget2_data_onload_func onload, em_async_wget2_data_onerror_func onerror, em_async_wget2_data_onprogress_func onprogress);
     //void emscripten_async_wget2_data(const char* url, const char* requesttype, const char* param, void *arg, int free, void (*onload)(void*, void*, unsigned), void (*onerror)(void*, int, const char*), void (*onprogress)(void*, int, int));
