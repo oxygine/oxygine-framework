9aa7ce07427f8544889789ed60dc8a7187d3cca2
dmuratshin <frankinshtein85@gmail.com>
2016-07-22 11:01:52 -0700

    minor

--- tools/resbuild/process_atlas.py
+++ tools/resbuild/process_atlas.py
@@ -450,7 +450,7 @@ def processRS(context, walker):
                 resize_filter = Image.BICUBIC
 
             if context.args.resize:
-                if as_bool(image_el.getAttribute("trueds")):
+                if as_bool(image_el.getAttribute("trueds")) or (not trim and not extend):
                     frame_image = frame_image.resize(
                         (frame_size[0], frame_size[1]), resize_filter)
                 else:
