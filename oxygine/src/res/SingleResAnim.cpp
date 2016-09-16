c440e895076e39b053aa7df9b25b02b39fda620c
dmuratshin <frankinshtein85@gmail.com>
2016-09-16 20:35:23 +0500

    custom ResAnim

---
+++ oxygine/src/res/SingleResAnim.cpp
@@ -0,0 +1,16 @@
+#include "SingleResAnim.h"
+
+namespace oxygine
+{
+	void makeAlpha(const ImageData& srcImage, Rect& bounds, std::vector<unsigned char>& alpha, HitTestData& adata, bool hittest);
+
+	void SingleResAnim::init(const std::string& file, int columns, int rows, float scaleFactor)
+	{
+
+	}
+
+	void SingleResAnim::init(Image* original, int columns, int rows, float scaleFactor)
+	{
+		makeAlpha
+	}
+}
---
+++ oxygine/src/res/SingleResAnim.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "ResAnim.h"
+
+namespace oxygine
+{
+	class SingleResAnim: public ResAnim
+	{
+	public:
+		void init(const std::string& file, int columns = 1, int rows = 1, float scaleFactor = 1.0f);
+		void init(Image* original, int columns = 1, int rows = 1, float scaleFactor = 1.0f);
+
+	protected:
+		std::vector<unsigned char> _data;
+	};
+}
