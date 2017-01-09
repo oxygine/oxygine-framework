e39c12331e901948ede1cb45dd82d2aeebc0ee76
dmuratshin <frankinshtein85@gmail.com>
2017-01-09 18:32:49 +0500

    min

--- oxygine/src/UpdateState.h
+++ oxygine/src/UpdateState.h
@@ -8,8 +8,13 @@ namespace oxygine
     public:
         UpdateState(): time(0), dt(0), iteration(0) {}
 
+        /**local time*/
         timeMS time;
+
+        /**delta time since last update*/
         timeMS dt;
+
+        /**current iteration, used with fixed Clock update */
         int iteration;
     };
 }
