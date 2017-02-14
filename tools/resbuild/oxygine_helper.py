e0701561f2f49fee905e89a0264116053e1b2440
dmuratshin <frankinshtein85@gmail.com>
2017-02-15 01:44:32 +0500

    added support to PVRTexTool 64bit

--- tools/resbuild/oxygine_helper.py
+++ tools/resbuild/oxygine_helper.py
@@ -1,6 +1,6 @@
 import os
 import sys
-
+import platform
 
 class helper(object):
 
@@ -9,12 +9,16 @@ class helper(object):
         self.path_process_xml = self.path_root + "tools/oxyresbuild.py"
         self.path_bmfont = self.path_root + "/3rdPartyTools/BMFont/bmfont.com"
 
+        bit64 = ""
+        if platform.machine().endswith('64'): 
+            bit64 = "_64"
+
         if sys.platform == "darwin":
             self.path_pvrtextool = self.path_root + "/3rdPartyTools/macosx/PVRTexToolCLI "
         elif sys.platform == "linux2":
-            self.path_pvrtextool = self.path_root + "/3rdPartyTools/linux/PVRTexToolCLI "
-        else:
-            self.path_pvrtextool = self.path_root + "/3rdPartyTools/PVRTexToolCLI.exe "
+            self.path_pvrtextool = self.path_root + "/3rdPartyTools/linux/PVRTexToolCLI{} ".format(bit64, )
+        else:            
+            self.path_pvrtextool = self.path_root + "/3rdPartyTools/PVRTexToolCLI{}.exe ".format(bit64, )
 
     def process_xml(self, embedded, args):
         if embedded:
