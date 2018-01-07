abb07c1a05646155e3cc8558a5e05daea580bfec
dmuratshin <frankinshtein85@gmail.com>
2018-01-07 21:59:26 +0500

    min

--- tools/gen_template.py
+++ tools/gen_template.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import os
 import io
 import glob
